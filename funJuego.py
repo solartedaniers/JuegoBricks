4279aa81158ac7319d7af73ec79b1ad4338ff15e
proyecMariana <guwisanchez123@umariana.edu.co>
2025-03-18 08:25:27 -0500

    Nuevo Script

---
+++ funJuego.py
@@ -0,0 +1,13 @@
+def iniciar_juego():
+    print("¡Iniciando juego!")
+
+def mover_personaje(direccion):
+    print(f"Moviendo personaje hacia {direccion}")
+
+def finalizar_juego():
+    print("¡Fin del juego!")
+
+# Ejecución
+iniciar_juego()
+mover_personaje("derecha")
+finalizar_juego()
